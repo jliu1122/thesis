6c958e88432faf824465219a5bc592fb9982714e
jliu1122 <jiahao.200101@example.com>
2025-06-28 22:00:36 +0000

    add function

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,31 @@
+import fasttext.util
+import numpy as np
+
 from pycocotools.coco import COCO
 
 class CocoDataSet:
 
-  def __init__(self, captions, instance):
-    self.instance = COCO(instance)
-    self.captions = COCO(captions)
+    def __init__(self, captions, instance):
+        self.instance = COCO(instance)
+        self.captions = COCO(captions)
 
-  def loadImgsFromCategories(self, cats):
-    """
-    Retrieves the images object based on the category names
+    def loadImgsFromCategories(self, cats):
+        """
+        Retrieves the images object based on the category names
 
-    Args:
-      cats (str): List of category names
+        Args:
+          cats (str): List of category names
+        
+        Return:
+          list: A list of image objects.
+        """
+        catIDs = self.instance.getCatIds(catNms=cats)
+        imgIDs = self.instance.getImgIds(catIds=catIDs)
+
+        return self.instance.loadImgs(imgIDs)
     
-    Return:
-      list: A list of image objects.
-    """
-    catIDs = self.instance.getCatIds(catNms=cats)
-    imgIDs = self.instance.getImgIds(catIds=catIDs)
-
-    return self.instance.loadImgs(imgIDs)
-
-  
+    def getImgFromCatetoryName(self, cats_name):
+        ids = self.instance.getCatIds(catNms=cats_name)
+        img_ids = self.instance.getImgIds(catIds=ids)
+        imgs = self.instance.loadImgs(img_ids)
+        return imgs
